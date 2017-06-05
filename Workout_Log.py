2f39a7d99723198b0fca26398f63cf96ab6b3348
robertdfrench <frenchrd@ornl.gov>
2017-06-05 13:23:08 -0400

    Automatically determine style from Class name

--- Workout_Log.py
+++ Workout_Log.py
@@ -3,9 +3,9 @@
 class Exercise(object):
     'Super class of exercises that will include calisthenics, weights, and cardio.'
 
-    def __init__(self, name, style, date, notes):
+    def __init__(self, name, date, notes):
         self.name = name
-        self.style = style
+        self.style = self.__class__.__name__
         self.date = date
         self.notes = notes
 
@@ -16,8 +16,8 @@ class Exercise(object):
 class Weighted(Exercise):
     'Weight sublcass includes exercises that required weight'
 
-    def __init__(self, name, style, date, notes, reps, weight, sets):
-        super().__init__(name, style, date, notes)
+    def __init__(self, name, date, notes, reps, weight, sets):
+        super().__init__(name, date, notes)
         self.reps = reps
         self.weight = weight
         self.sets = sets
@@ -26,22 +26,22 @@ class Weighted(Exercise):
 class Calisthenic(Exercise):
     "Calisthenic sublcass includes exercises that don't require weight and aren't cardio"
     
-    def __init__(self, name, style, date, notes, reps, sets):
-        super().__init__(name, style, date, notes)
+    def __init__(self, name, date, notes, reps, sets):
+        super().__init__(name, date, notes)
         self.reps = reps
         self.sets = sets
 
 class Cardio(Exercise):
     "Cardio sublcass includes exercises that get the body moving"
     
-    def __init__(self, name, style, date, notes, duration, distance):
-        super().__init__(name, style, date, notes)
+    def __init__(self, name, date, notes, duration, distance):
+        super().__init__(name, date, notes)
         self.duration = duration
         self.distance = distance
 
 def Main():
-    KnownExercises = [Calisthenic('Pushup', "Calisthenic", "June 5", "Got swole", "25", "5"),
-                      Weighted("Bench Press", "Weighted", "June 4","get big", "5", "135", "3")
+    KnownExercises = [Calisthenic('Pushup', "June 5", "Got swole", "25", "5"),
+                      Weighted("Bench Press", "June 4","get big", "5", "135", "3")
                       ]
 
     for exercise in KnownExercises:
